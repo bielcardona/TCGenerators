3f8b0e1c8f7e1f06b59a3aeab709e96f7139cbaf
Biel Cardona <bielcardona@gmail.com>
2019-02-14 10:39:28 +0100

    improved output of scripts

--- compute_offspring.py
+++ compute_offspring.py
@@ -2,6 +2,7 @@ import sys
 import phylonetwork
 from generating_TC import BTC_offspring_generator
 import multiprocessing
+import time
 
 
 def treat_line(line):
@@ -21,13 +22,22 @@ if __name__ == '__main__':
         exit()
 
     pool = multiprocessing.Pool()
+    start = time.time()
 
     with open(filein, 'r') as f, open(fileout, 'w') as g:
         lines = f.readlines()
+        numlines = len(lines)
         outs = pool.imap_unordered(treat_line, lines)
+        proclines = 0
         numnets = 0
         for out in outs:
+            proclines += 1
+            numnets += len(out)
+            now = time.time()
+            print(f"Processed {proclines} of {len(lines)} lines. "+
+            f"Elapsed time: {now-start:.2f}s Expected time: {(now-start)*numlines/proclines:.2f}s " +
+            f"Network count: {numnets}. " +
+            f"Ratio: {numnets / proclines:.2f} Expected: {int(numnets*numlines/proclines)}")
             for l in out:
-                numnets += 1
                 g.write(l+"\n")
-            print(f"Currently {numnets}")
+        print(f"Total offpring: {numnets} networks")
--- count_offspring.py
+++ count_offspring.py
@@ -2,6 +2,7 @@ import sys
 import phylonetwork
 from generating_TC import count_feasible_pairs
 import multiprocessing
+import time
 
 
 def treat_line(line):
@@ -18,9 +19,11 @@ if __name__ == '__main__':
         exit()
 
     pool = multiprocessing.Pool()
+    start = time.time()
 
     with open(filein,'r') as f:
         lines = f.readlines()
+        numlines = len(lines)
         outs = pool.imap_unordered(treat_line,lines)
         proclines = 0
 
@@ -28,4 +31,9 @@ if __name__ == '__main__':
         for out in outs:
             proclines += 1
             numnets += out
-            print(f"Processed {proclines} of {len(lines)} lines. Network count: {numnets}. Ratio: {numnets / proclines:.2f}")
+            now = time.time()
+            print(f"Processed {proclines} of {len(lines)} lines. "+
+            f"Elapsed time: {now-start:.2f}s Expected time: {(now-start)*numlines/proclines:.2f}s " +
+            f"Network count: {numnets}. " +
+            f"Ratio: {numnets / proclines:.2f} Expected: {int(numnets*numlines/proclines)}")
+        print(f"Total offpring: {numnets} networks")
